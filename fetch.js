a32746c7c675e3a862955be81c2b69ef4e4d65af
Johnathan Law <johnathan.law@matillion.com>
2024-10-17 17:36:02 +0100

    chore: prep for moving to workato

--- fetch.js
+++ fetch.js
@@ -1,7 +1,9 @@
+exports.main = async () => {
 // Fetch the file specified below, and return its content. See https://github.com/johnathanlaw/rotabot/blob/main/fetch.js
-const fileUrl = 'https://raw.githubusercontent.com/johnathanlaw/rotabot/main/config.json';
+const fileUrl = 'https://raw.githubusercontent.com/johnathanlaw/rotabot/refs/heads/main/workato-config.json';
 const res = await fetch(fileUrl);
 
 // Return the configuration
 const config = await res.json();
 return config;
+}
--- format.js
+++ format.js
@@ -1,8 +1,12 @@
-const active = (inputData['days-active'][inputData['day']].toLowerCase() === 'y');
-let msg = inputData['unformatted'];
+exports.main = async ({ day, days_active, unformatted, channels, cells }) => {
+const active = (days_active[day].toLowerCase() === 'y');
+const ASCII_A = 'A'.charCodeAt(0); // 65
+
+let msg = unformatted;
 
 for (let i = 0; i < 10; i++) {
-    msg = msg.replace(`<<${i}>>`, inputData[`${i}`]);
+  let current_col = `col_${String.fromCharCode(ASCII_A+i)}`
+  msg = msg.replace(`<<${current_col}>>`, `${cells[current_col]}`);
 }
-
-return { message: msg, channel: inputData['channel'], active: active };
+return { message: msg, channel: channels, active: active };
+}
